004e61d619f7b3d90425159056e1acd047112a9d
leiandi <43455219+leiandi@users.noreply.github.com>
2018-12-08 17:20:46 +0300

    Add files via upload

---
+++ problem.2/main.cpp
@@ -0,0 +1,18 @@
+//Name: Jameson Villaluna
+//Date: October 7, 2018
+#include <iostream>
+#include <string>
+using std::cout;
+using std::cin;
+using std::string;
+
+int main() {
+    float daily_budget, total_budget;
+    cout << "Please enter your daily budget: ";
+    cin >> daily_budget;
+    total_budget = daily_budget*7;
+    cout << "the product of daily budget is " << total_budget << "\n";
+    cout << "the daily budget is " << daily_budget;
+  system("pause");
+
+}
